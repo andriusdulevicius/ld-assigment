a1e593281e237c74ffdfe1ebb93df04ca7259f5d
AndriusDulevicius <andriusdulevicius@email.com>
2022-03-06 13:01:15 +0200

    simple testing added

---
+++ src/components/__tests__/Header.test.jsx
@@ -0,0 +1,19 @@
+import Header from '../Header';
+import { screen, render } from '@testing-library/react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../../styles/theme';
+
+describe('Header', () => {
+  it('should render correctly', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Header />
+      </ThemeProvider>
+    );
+
+    expect(screen.getByText(/whats new/i)).toBeTruthy();
+    expect(screen.getByText(/welcome/i)).toBeTruthy();
+    expect(screen.getByText(/welcomeapp.vetrina.it/i)).toBeTruthy();
+    expect(screen.getByRole('button')).toBeTruthy();
+  });
+});
